bc76f7d3a96f8eb61a50b74f4eef7076161c5e24
Harsh Deshpande <harshdeshpande100@gmail.com>
2021-05-19 14:59:47 +0530

    feat: added header component and type

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,36 @@
+import { HeaderProps } from "./HeaderProps.types";
+
+export function Header({ username, score }: HeaderProps) {
+  return (
+    <>
+      <div className="nav-main nav-primary">
+        <h1>
+          💠 Log<sub>N</sub>Quiz
+        </h1>
+        <ul className="list nav--list">
+          <li className="nav-item">
+            {/* <NavLink
+              end
+              to="/products"
+              className="NavElement"
+              activeStyle={{
+                fontWeight: "bold",
+              }}
+            >
+              PRODUCTS
+            </NavLink> */}
+            Quiz 1
+          </li>
+          <li className="nav-item">
+            Quiz 2
+          </li>
+        </ul>
+      </div>
+
+      <div style={{ fontSize: "1.2rem" }}>
+        <div> Welcome, {username}! </div>
+        <div> Score: {score} </div>
+      </div>
+    </>
+  );
+}
---
+++ src/Components/Header/HeaderProps.types.ts
@@ -0,0 +1,5 @@
+export type HeaderProps = {
+    username: string;
+    score: number;
+  };
+  
