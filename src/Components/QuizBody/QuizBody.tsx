5e74fadc6278cbc1762861d1dadd8f694065c533
Harsh Deshpande <harshdeshpande100@gmail.com>
2021-05-23 18:07:02 +0530

    change in reset button

--- src/Components/QuizBody/QuizBody.tsx
+++ src/Components/QuizBody/QuizBody.tsx
@@ -81,7 +81,7 @@ export function QuizBody() {
                     setCurrentQueNo(1);
                 }}
             >
-                ⭯
+                ↺       
       </button>
         </>
     );
